2ccad14209f284a29d45e3bb4118eb2db8222cb0
Chidume Nnamdi <kurtwanger40@gmail.com>
2024-11-26 16:24:23 +0100

    Upgrade to Angular 19

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -20,7 +20,7 @@ import {CourseImageComponent} from '../course-image/course-image.component';
     selector: 'course-card',
     templateUrl: './course-card.component.html',
     styleUrls: ['./course-card.component.css'],
-    changeDetection: ChangeDetectionStrategy.Eager,
+    changeDetection: ChangeDetectionStrategy.OnPush,
     standalone: false
 })
 export class CourseCardComponent implements OnInit, AfterViewInit, AfterContentInit {
--- src/app/course-image/course-image.component.ts
+++ src/app/course-image/course-image.component.ts
@@ -4,7 +4,7 @@ import {Component, Input, OnInit, ChangeDetectionStrategy} from '@angular/core';
     selector: 'course-image',
     templateUrl: './course-image.component.html',
     styleUrls: ['./course-image.component.css'],
-    changeDetection: ChangeDetectionStrategy.Eager,
+    changeDetection: ChangeDetectionStrategy.OnPush,
     standalone: false
 })
 export class CourseImageComponent implements OnInit {
