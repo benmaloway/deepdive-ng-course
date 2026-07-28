a7cc16c6b07b04f5db23f96578323d351c3167fe
Benjamin Maloway <benmaloway@gmail.com>
2026-07-28 12:52:32 +0200

    S2.25

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,23 @@ import {COURSES} from '../db-data';
 import { Course } from './model/course';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    changeDetection: ChangeDetectionStrategy.Default,
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.Default,
+  standalone: false
 })
+
 export class AppComponent {
 
-    courses = COURSES;
+  courses = COURSES;
 
-    onCourseSelected(course: Course) {
-        console.log("Card selected: " + course.description);
-    }
+  trackCourse(index: number, course: Course) {
+    return course.id;
+  }
+
+  onCourseSelected(course: Course) {
+    console.log("Card selected: " + course.description);
+  }
 
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -10,11 +10,36 @@ export class CourseCardComponent {
   @Input({ required: true })
   course: Course;
 
+  @Input({ required: true })
+  index: number;
+
+  // Used with old ngFor syntax example.
+  @Input()
+  cardIndex: number;
+
   @Output()
   courseSelected = new EventEmitter<Course>();
 
-   onCourseViewed() {
-    console.log("Course viewed: " + this.course.description);
-    this.courseSelected.emit(this.course);
-   }
+  onCourseViewed() {
+  console.log("Course viewed: " + this.course.description);
+  this.courseSelected.emit(this.course);
+  }
+
+  cardClasses() {
+    if (this.course.category === 'BEGINNER') {
+      return {
+        'beginner': true
+      }
+    }
+    /* return {
+      'beginner': this.course.category === 'BEGINNER'
+    } */
+  }
+
+  cardStyles() {
+
+    return {
+      'text-decoration': 'underline'
+    }
+  }
 }
