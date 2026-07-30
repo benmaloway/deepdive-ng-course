efbdafa35d16ce612ff8d9738d4d1169257c291e
Benjamin Maloway <benmaloway@gmail.com>
2026-07-30 11:04:30 +0200

    Finish S4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,25 +18,7 @@ export class AppComponent implements AfterViewInit {
 
   title = COURSES[0].description;
 
-  startDate = new Date(2000, 0, 1);
-
   number = 9.99;
-
-
-
-  // This decorator gets the first instance of the CourseCardComponent in the view and assigns it to the card property.
-  @ViewChild(CourseCardComponent)
-  card1: CourseCardComponent;
-  
-  // Returns the reference to the component instance of the CourseCardComponent in the view and assigns it to the card2 property.
-  @ViewChild('cardRef2')
-  card2: CourseCardComponent;
-
-  // Returns the reference to the container dom native element.
-
-  @ViewChild('cardRef3', { read: ElementRef })
-  card3: ElementRef;
-
 
   // Returns the reference to the container div element in the view and assigns it to the containerDiv property.
   // You cannot use this to get deeper children of the component. You can only get the first level child elements of the component.
@@ -44,8 +26,8 @@ export class AppComponent implements AfterViewInit {
   containerDiv: ElementRef;
 
   // This returns a QueryList of all the CourseCardComponent instances in the view and assigns it to the cards property. You can use this to get all the child components of a specific type in the view.
-  @ViewChildren(CourseCardComponent)
-  cards: QueryList<CourseCardComponent>;
+  @ViewChildren(CourseCardComponent, { read: ElementRef })
+  cards: QueryList<ElementRef>;
 
   constructor() {
   }
@@ -72,18 +54,7 @@ export class AppComponent implements AfterViewInit {
       category: 'Testing',
       lessonsCount: 10
     });
-    // this.courses = [...this.courses, {
-    /* this.courses = [
-      ...this.courses,
-      {
-        id: 5,
-        description: 'ADDED Angular Testing Course',
-        iconUrl: 'https://angular-academy.s3.amazonaws.com/thumbnails/angular-testing.png',
-        longDescription: 'Learn to unit test and e2e test your Angular apps using Jasmine, Karma and Protractor',
-        category: 'Testing',
-        lessonsCount: 10
-      }
-    ]; */
+
   }
 
   trackCourse(index: number, course: Course) {
@@ -91,9 +62,7 @@ export class AppComponent implements AfterViewInit {
   }
 
   onCourseSelected(course: Course) {
-    console.log(this.card1);
-    console.log(this.card2);
-    console.log(this.card3);
+    console.log(this.cards.first);
     console.log("Container Div:", this.containerDiv);
   }
 
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,28 +1,57 @@
-import { Component, ContentChild, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ContentChild, ContentChildren, ElementRef, EventEmitter, Input, OnInit, Output, QueryList, ViewChild } from '@angular/core';
 import { Course } from '../model/course';
+import { CourseImageComponent } from '../course-image/course-image.component';
+
 
 @Component({
   selector: 'course-card',
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css'
 })
-export class CourseCardComponent {
-  @Input({ required: true })
-  course: Course;
+export class CourseCardComponent implements OnInit, AfterViewInit, AfterContentInit {
 
   @Input({ required: true })
   index: number;
 
-  // Used with old ngFor syntax example.
+
   @Input()
   cardIndex: number;
 
   @Output()
   courseSelected = new EventEmitter<Course>();
 
-  //To get the contents of projected content, you cannot use ViewChild
-  @ContentChild('courseImageDiv')
-  image: ElementRef;
+  //To get the contents of projected content, you cannot use ViewChild. This queries a template reference to 'container'
+  // @ContentChild('container')
+  // image;
+
+  // To get the contents of projected content as a component instance.
+  @ContentChild(CourseImageComponent, { read: ElementRef })
+  image : ElementRef;
+
+  // To get the first index of a list of projected content.
+  @ContentChildren(CourseImageComponent, { read: ElementRef })
+  images : QueryList<ElementRef>;
+
+  // Must use this lifecycle hook to access the projected content when all content has been initialized. You cannot access it in the constructor or ngOnInit.
+  AfterContentInit() {
+    console.log("CourseCardComponent content initialized", this.image); 
+    console.log("CourseCardComponent content initialized", this.images);
+
+  }
+
+  @Input({ required: true })
+  course: Course;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // initialization logic if needed
+    console.log('CourseCardComponent initialized');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('CourseCardComponent content initialized');
+  }
 
   onCourseViewed() {
     //console.log("Course viewed: " + this.course.description);
