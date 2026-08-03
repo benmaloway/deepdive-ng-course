01931af878324861f287065c28395c896aaf8fd2
Benjamin Maloway <benmaloway@gmail.com>
2026-08-03 13:40:22 +0200

    S7 finish view encapsulation.

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -16,6 +16,17 @@ import {COURSES} from '../../db-data';
 import {Course} from '../model/course';
 import {CourseImageComponent} from '../course-image/course-image.component';
 
+// default : encapsulation: ViewEncapsulation.Emulated
+// This is the default mode and uses Angular's built-in emulation of the Shadow DOM to encapsulate styles. This means that styles defined in a component will only affect that component and not any other components. This is the most common form of encapsulation used in Angular.
+
+// none : encapsulation: ViewEncapsulation.None
+// None negates need for :host-context, ::host and ::ng-deep but also removes encapsulation so styles can bleed into other components.  Use with caution.
+
+
+// shadowDom : encapsulation: ViewEncapsulation.ShadowDom
+// The shadowDom encapsulation mode uses the browser's native shadow DOM implementation to encapsulate styles. This means that styles defined in a component will not affect any other components, and styles defined outside of the component will not affect the component. This is the most secure form of encapsulation, but it may not be supported in all browsers.
+
+// Do not use ViewEncapsulation.Native as it is deprecated and will be removed in future versions of Angular.  Use ViewEncapsulation.ShadowDom instead.
 @Component({
     selector: 'course-card',
     templateUrl: './course-card.component.html',
